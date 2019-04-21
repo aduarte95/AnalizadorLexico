e759c2ef37236e8733dd5d8b0461a8719f834c6d
Alexa Duarte <alexa-2095@hotmail.com>
2019-04-20 22:42:40 -0600

    Diferencia entre doc y consulta

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
  
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("clean_text.txt"));; //Escribe en archivo limpio
+            BufferedWriter writer;
+            TokenPersonalizado token;
             String archivo;
             String option = "-d"; //Por defecto se revisa un documento
             
@@ -20,25 +21,46 @@ public class Main {
            
             // Se trata de leer el archivo y analizarlo en la clase que se ha creado con JFlex
             BufferedReader buffer = new BufferedReader(new FileReader(archivo));
-            AnalizadorLexicoDoc analizadorJFlex = new AnalizadorLexicoDoc(buffer);
-
-            while(true){
-                // Obtener el token analizado y mostrar su información
-                TokenPersonalizado token = analizadorJFlex.yylex();
-                
-                if(!analizadorJFlex.existenTokens())
-                    break;
-
-                System.out.println(token.toString()); //Imprime lo que analiza
-                
-                if((token.getToken()).equals("Termino")) { //Si es un término lo escribe en el archivo.
-                    writer.write(token.getLexema());
-                    writer.newLine();
-                }
-            }
             
-            writer.close(); 
-        
+            if(!option.equals("-c")) { //Sino hay parámetro -c se analiza como un doc
+                AnalizadorLexicoDoc analizadorJFlex = new AnalizadorLexicoDoc(buffer);
+                writer = new BufferedWriter(new FileWriter("clean_text_doc.txt"));; //Escribe en archivo limpio
+                
+                while(true){
+                    // Obtener el token analizado y mostrar su información
+                    token = analizadorJFlex.yylex();
+                    
+                    if(!analizadorJFlex.existenTokens())
+                        break;
+
+                    System.out.println(token.toString()); //Imprime lo que analiza
+                    
+                    if((token.getToken()).equals("Termino")) { //Si es un término lo escribe en el archivo.
+                        writer.write(token.getLexema());
+                        writer.newLine();
+                    }
+                }
+                writer.close(); 
+            } else { //Analizar consulta
+                AnalizadorLexicoCon analizadorJFlex = new AnalizadorLexicoCon(buffer);
+                writer = new BufferedWriter(new FileWriter("clean_text_con.txt"));; //Escribe en archivo limpio
+                
+                while(true){
+                    // Obtener el token analizado y mostrar su información
+                    token = analizadorJFlex.yylex();
+                    
+                    if(!analizadorJFlex.existenTokens())
+                        break;
+
+                    System.out.println(token.toString()); //Imprime lo que analiza
+                    
+                    if((token.getToken()).equals("Termino")) { //Si es un término lo escribe en el archivo.
+                        writer.write(token.getLexema());
+                        writer.newLine();
+                    }
+                }
+                writer.close(); 
+            }
         } 
         catch (Exception e) {
             System.out.println(e.toString());
